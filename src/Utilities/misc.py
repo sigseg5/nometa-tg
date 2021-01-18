e3074c65141bfbe515688555d196e3fd2f580525
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2021-01-18 20:34:34 +0500

    Added func for sending docs from server

--- src/Utilities/misc.py
+++ src/Utilities/misc.py
@@ -1,4 +1,6 @@
-from os import remove
+from os import remove, getenv
+from telegram.ext import CallbackContext
+from telegram.update import Update
 
 
 def remove_original_doc_from_server(logger, update):
@@ -10,3 +12,31 @@ def remove_original_doc_from_server(logger, update):
     except Exception:
         logger.error("Can't remove original file")
         update.message.reply_text("Error at removing original file from server")
+
+
+def send_file(logger, update: Update, context: CallbackContext, mode):
+    FAWKES_MODE = getenv("FAWKES_MODE")
+
+    logger.info("Preparing for sending cloaked file\n")
+
+    if mode == "cloaked":
+        try:
+            _ = context.bot.send_document(chat_id=update.effective_message.chat_id,
+                                          document=open('documents/clean_image_{0}_cloaked.png'.format(FAWKES_MODE), 'rb'))
+            logger.info("Cloaked file sending finished")
+        except Exception as e:
+            logger.error(e)
+            logger.critical("EXCEPTION at cloaked file sender section")
+            update.message.reply_text("Error at sending cloaked file")
+
+    elif mode == "clean":
+        try:
+            _ = context.bot.send_document(chat_id=update.effective_message.chat_id,
+                                          document=open('documents/clean_image.jpg', 'rb'))
+            logger.info("Document without metadata sending finished")
+            update.message.reply_text("Metadata removed from photo")
+            logger.info("Document sending finished")
+        except Exception as e:
+            logger.error(e)
+            logger.critical("EXCEPTION at file sender section for remove metadata")
+            update.message.reply_text("Error at sending clean file")
