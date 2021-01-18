2906de5b8c6fd18d66126f3ce72a890b40fa2906
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2021-01-18 18:33:46 +0500

    jpg files are prop supported again

--- src/handlers/document_handler.py
+++ src/handlers/document_handler.py
@@ -7,6 +7,7 @@ from telegram.ext import CallbackContext
 from telegram.update import Update
 
 from src.Utilities.metadata_worker import delete_metadata
+from src.Utilities.metadata_worker import delete_metadata_from_png
 from src.Utilities.cmd_logger import result_of
 
 SUPPORTED_MIME_LIST = ("image/jpeg", "image/png")
@@ -64,83 +65,86 @@ def document_handler(update: Update, context: CallbackContext):
     else:
         # TODO: add png section
         logger.info("Metadata removing started")
-        delete_metadata("documents/image.jpg")
-        logger.info("Metadata was successfully deleted")
-        update.message.reply_text("Metadata was successfully deleted")
-
-        logger.info("Preparing for original file deletion on server")
-        try:
-            remove("documents/image.jpg")
-            update.message.reply_text("Original file successfully removed from server")
-            logger.info("Original file successfully removed")
-        except Exception:
-            logger.error("Can't remove original file")
-            update.message.reply_text("Error at removing original file from server")
-
-        try:
-            logger.info("Goes into fawkes section")
-            update.message.reply_text("Applying face hider tools, wait...")
-            _ = call(["fawkes", "-d", "documents", "--mode", FAWKES_MODE])
-            logger.info(result_of("ls documents"))
-            if path.exists('documents/clean_image_{0}_cloaked.png'.format(FAWKES_MODE)):
-                is_faces_found = True
-
-            logger.info("Does faces found?: {}".format(is_faces_found))
-            logger.info("fawkes try-catch finished")
-
-        except Exception as e:
-            logger.error(e)
-            logger.critical("EXCEPTION at fawkes section")
-            update.message.reply_text("Error at hiding faces")
-
-        if is_faces_found:
-            logger.info("Preparing for sending cloaked file\n")
-            try:
-                _ = context.bot.send_document(chat_id=update.effective_message.chat_id,
-                                              document=open('documents/clean_image_{0}_cloaked.png'.format(FAWKES_MODE),
-                                                            'rb'))
-                logger.info("Cloaked file sending finished")
-            except Exception as e:
-                logger.error(e)
-                logger.critical("EXCEPTION at cloaked file sender section")
-                update.message.reply_text("Error at sending cloaked file")
-
-            logger.info("Preparing for clean file deletion on server")
-            try:
-                remove("documents/clean_image.jpg")
-                update.message.reply_text("Clean version of file successfully removed from server")
-                logger.info("Clean version of file successfully removed")
-            except Exception:
-                logger.error("Can't remove clean version of file")
-                update.message.reply_text("Error at removing clean version of file from server")
-
-            logger.info("Preparing for cloaked photo deletion on server")
-            try:
-                remove("documents/clean_image_{0}_cloaked.png".format(FAWKES_MODE))
-                update.message.reply_text("Cloaked file successfully removed from server")
-                logger.info("Cloaked file successfully removed")
-            except Exception:
-                logger.error("Can't remove cloaked file")
-                update.message.reply_text("Error at removing cloaked file from server")
+        if kind.mime == "image/png":
+            logger.info("This is PNG")
         else:
-            logger.info("No faces found")
-            update.message.reply_text("Can't find any faces")
-            logger.info("Preparing for sending photo without metadata")
+            delete_metadata("documents/image")
+            logger.info("Metadata was successfully deleted")
+            update.message.reply_text("Metadata was successfully deleted")
+
+            logger.info("Preparing for original file deletion on server")
             try:
-                _ = context.bot.send_document(chat_id=update.effective_message.chat_id,
-                                              document=open('documents/clean_image.jpg', 'rb'))
-                logger.info("Document without metadata sending finished")
-                update.message.reply_text("Metadata removed from photo")
-                logger.info("Photo sending finished")
-            except Exception as e:
-                logger.error(e)
-                logger.critical("EXCEPTION at file sender section for remove metadata")
-                update.message.reply_text("Error at sending file")
+                remove("documents/image")
+                update.message.reply_text("Original file successfully removed from server")
+                logger.info("Original file successfully removed")
+            except Exception:
+                logger.error("Can't remove original file")
+                update.message.reply_text("Error at removing original file from server")
 
             try:
-                remove("documents/clean_image.jpg")
-                update.message.reply_text("File without metadata successfully removed from server")
-                logger.info("File without metadata successfully removed")
-            except Exception:
-                logger.error("Can't remove file without metadata")
-                update.message.reply_text("Error at removing file without metadata from server")
+                logger.info("Goes into fawkes section")
+                update.message.reply_text("Applying face hider tools, wait...")
+                _ = call(["fawkes", "-d", "documents", "--mode", FAWKES_MODE])
+                logger.info(result_of("ls documents"))
+                if path.exists('documents/clean_image_{0}_cloaked.png'.format(FAWKES_MODE)):
+                    is_faces_found = True
+
+                logger.info("Does faces found?: {}".format(is_faces_found))
+                logger.info("fawkes try-catch finished")
+
+            except Exception as e:
+                logger.error(e)
+                logger.critical("EXCEPTION at fawkes section")
+                update.message.reply_text("Error at hiding faces")
+
+            if is_faces_found:
+                logger.info("Preparing for sending cloaked file\n")
+                try:
+                    _ = context.bot.send_document(chat_id=update.effective_message.chat_id,
+                                                  document=open('documents/clean_image_{0}_cloaked.png'.format(FAWKES_MODE),
+                                                                'rb'))
+                    logger.info("Cloaked file sending finished")
+                except Exception as e:
+                    logger.error(e)
+                    logger.critical("EXCEPTION at cloaked file sender section")
+                    update.message.reply_text("Error at sending cloaked file")
+
+                logger.info("Preparing for clean file deletion on server")
+                try:
+                    remove("documents/clean_image.jpg")
+                    update.message.reply_text("Clean version of file successfully removed from server")
+                    logger.info("Clean version of file successfully removed")
+                except Exception:
+                    logger.error("Can't remove clean version of file")
+                    update.message.reply_text("Error at removing clean version of file from server")
+
+                logger.info("Preparing for cloaked photo deletion on server")
+                try:
+                    remove("documents/clean_image_{0}_cloaked.png".format(FAWKES_MODE))
+                    update.message.reply_text("Cloaked file successfully removed from server")
+                    logger.info("Cloaked file successfully removed")
+                except Exception:
+                    logger.error("Can't remove cloaked file")
+                    update.message.reply_text("Error at removing cloaked file from server")
+            else:
+                logger.info("No faces found")
+                update.message.reply_text("Can't find any faces")
+                logger.info("Preparing for sending photo without metadata")
+                try:
+                    _ = context.bot.send_document(chat_id=update.effective_message.chat_id,
+                                                  document=open('documents/clean_image.jpg', 'rb'))
+                    logger.info("Document without metadata sending finished")
+                    update.message.reply_text("Metadata removed from photo")
+                    logger.info("Photo sending finished")
+                except Exception as e:
+                    logger.error(e)
+                    logger.critical("EXCEPTION at file sender section for remove metadata")
+                    update.message.reply_text("Error at sending file")
+
+                try:
+                    remove("documents/clean_image.jpg")
+                    update.message.reply_text("File without metadata successfully removed from server")
+                    logger.info("File without metadata successfully removed")
+                except Exception:
+                    logger.error("Can't remove file without metadata")
+                    update.message.reply_text("Error at removing file without metadata from server")
