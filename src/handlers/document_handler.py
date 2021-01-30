70a4e93ca4a9af693f5afb6198ef62540ce11c69
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2021-01-30 23:08:39 +0500

    Now PNG files prop supported

--- src/handlers/document_handler.py
+++ src/handlers/document_handler.py
@@ -19,7 +19,7 @@ FAWKES_MODE = getenv("FAWKES_MODE")
 def document_handler(update: Update, context: CallbackContext):
     """
         This function has multiple scope of responsibility:
-        1. Downloading a document as '.jpg', generally this is best solution for compatibility;
+        1. Downloading a document without extension;
         2. Guessing a file type. If type isn't supported file removes from server;
         3. Checking is this file type supported by application. If type isn't supported file removes from server;
         4. Removing metadata from document via calling 'metadata_worker.py';
@@ -67,10 +67,73 @@ def document_handler(update: Update, context: CallbackContext):
     else:
         logger.info("Metadata removing started")
         if kind.mime == "image/png":
-            logger.info("This is PNG")
+            try:
+                delete_metadata_from_png("documents/image")
+                logger.info("Metadata was successfully deleted")
+            except Exception as e:
+                logger.error("Metadata wasn't deleted")
+                logger.error(e.args)
+                remove_original_doc_from_server(logger, update)
+                update.message.reply_text("Error at removing metadata from PNG file\nFile removed from server")
+
             remove_original_doc_from_server(logger, update)
-            logger.error("image/png isn't supported yet")
-            return
+
+            try:
+                logger.info("Goes into fawkes section")
+                update.message.reply_text("Applying face hider tools, wait...")
+                _ = call(["fawkes", "-d", "documents", "--mode", FAWKES_MODE])
+                logger.info(result_of("ls documents"))
+                if path.exists('documents/clean_image_{0}_cloaked.png'.format(FAWKES_MODE)):
+                    is_faces_found = True
+
+                logger.info("Does faces found?: {}".format(is_faces_found))
+                logger.info("fawkes try-catch finished")
+
+            except Exception as e:
+                logger.error("EXCEPTION at fawkes section")
+                logger.error(e.args)
+                update.message.reply_text("Error at hiding faces")
+
+            if is_faces_found:
+                logger.info("Preparing for sending cloaked file")
+
+                send_file(logger, update, context, "cloaked")
+
+                logger.info("Preparing for clean file deletion on server")
+                try:
+                    remove("documents/clean_image.png")
+                    update.message.reply_text("Clean version of file successfully removed from server")
+                    logger.info("Clean version of file successfully removed")
+                except Exception as e:
+                    logger.error("Can't remove clean version of file")
+                    logger.error(e.args)
+                    update.message.reply_text("Error at removing clean version of file from server")
+
+                logger.info("Preparing for cloaked photo deletion on server")
+                try:
+                    remove("documents/clean_image_{0}_cloaked.png".format(FAWKES_MODE))
+                    update.message.reply_text("Cloaked file successfully removed from server")
+                    logger.info("Cloaked file successfully removed")
+                except Exception as e:
+                    logger.error("Can't remove cloaked file")
+                    logger.error(e.args)
+                    update.message.reply_text("Error at removing cloaked file from server")
+            else:
+                logger.info("No faces found")
+                update.message.reply_text("Can't find any faces")
+                logger.info("Preparing for sending photo without metadata")
+
+                send_file(logger, update, context, "clean")
+
+                try:
+                    remove("documents/clean_image.png")
+                    update.message.reply_text("File without metadata successfully removed from server")
+                    logger.info("File without metadata successfully removed")
+                except Exception as e:
+                    logger.error("Can't remove file without metadata")
+                    logger.error(e.args)
+                    update.message.reply_text("Error at removing file without metadata from server")
+
         else:
             delete_metadata("documents/image")
             logger.info("Metadata was successfully deleted")
